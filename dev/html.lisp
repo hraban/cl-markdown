90b88467baa480e44c2e68409c3be0ed0a8788ce
Gary King <gwking@metabang.com>
2006-05-17 18:48:04 +0000

    Restarted HTML converter
    darcs-hash:fa37ea8233914e571869fca1edbf2b723cc95ba6

--- dev/html.lisp
+++ dev/html.lisp
@@ -1,109 +1,159 @@
 (in-package #:cl-markdown)
 
-(defparameter *markup->html* (make-container 
-                              'simple-associative-container
-                              :test #'equal
-                              :initial-contents 
-                              '((paragraph) (nil "<P>" "</P>" nil)
-                                (header1)    (nil "<H1>" "</H1>" nil)
-                                (bullet)    ("<UL>" "<LI>" "</LI>" "</UL>")
-                                (bullet paragraph)    ("<UL>" "<LI><P>" "</P></LI>" "</UL>")
-                                (code) ("<PRE><CODE>" nil nil "</CODE></PRE>")
-                                (number)    ("<OL>" "<LI>" "</LI>" "</OL>")
-                                (number paragraph)    ("<OL>" "<LI><P>" "</P></LI>" "</OL>")
-                                (paragraph quote) ("<BLOCKQUOTE>" "<P>" "</P>" "</BLOCKQUOTE>")
-                                (quote) ("<BLOCKQUOTE>" nil nil "</BLOCKQUOTE>")
-                                
-                                code (nil "<CODE>" "</CODE>" nil)
-                                strong (nil "<STRONG>" "</STRONG>" nil)
-                                emphasis (nil "<EM>" "</EM" nil))))
+;;; dealing with 'levels'
+
+(defparameter *current-document* nil)
+
+#+Ignore
+(defun d (text)
+  (let* ((document (markdown text))
+        (*current-document* document))
+    (setf (level document) 0
+          (markup document) nil)
+    (collect-elements (chunks document))))
 
 ;;; ---------------------------------------------------------------------------
 
-(defgeneric render (document style stream)
-  )
+(defparameter *markup->html*
+  (make-container 
+   'simple-associative-container
+   :test #'equal
+   :initial-contents 
+   '((header1)    (nil :h1)
+     (header2)    (nil :h2)
+     (header3)    (nil :h3)
+     (header4)    (nil :h4)
+     (header5)    (nil :h5)
+     (header6)    (nil :h6)
+     
+     (bullet)     ((:ul) :li)
+     (code)       ((:pre :code) nil)
+     (number)     ((:ol) :li)
+     (quote)      ((:blockquote) nil)
+     (horizontal-rule) (nil :hr))))
 
 ;;; ---------------------------------------------------------------------------
 
-(defgeneric render-to-html (thing)
-  )
-
-;;; ---------------------------------------------------------------------------
-
-(defvar *html-environment* nil)
-  
 (defmethod render ((document document) (style (eql :html)) stream)
-  (let ((*html-environment* nil))
-    (lml2:html-stream 
-     stream
-     ;;?? header
-     (:body
-      (iterate-elements 
-       (chunks document)
-       (lambda (chunk)
-         (render-to-html chunk)))))))
+  (let ((*current-document* document))
+    (setf (level document) 0
+          (markup document) nil)
+    (let* ((chunks (collect-elements (chunks document)))
+           (result (html-list->tree chunks)))
+      (if stream
+        (format stream "~S" result)
+        result))))
+
+;;; ---------------------------------------------------------------------------
+
+(defmethod render ((document document) (style (eql :html)) stream)
+  (eval `(html:html-stream 
+          ,stream 
+          (html:html ,@(render-to-stream document :html :none)))))
+
+;;; ---------------------------------------------------------------------------
+
+(defun html-marker (chunk)
+  (bind ((markup (markup-class-for-html chunk)))
+    (first markup)))
 
 ;;; ---------------------------------------------------------------------------
 
 (defmethod render-to-html ((chunk chunk))
-  (let ((finish (start-markup chunk)))
-    (iterate-elements
-     (lines chunk)
-     (lambda (line)
-       (render-to-html line)))
-    (safe-print finish)))
+  (bind ((block (collect-elements
+                 (lines chunk)
+                 :transform (lambda (line)
+                              (render-to-html line))))
+         (markup (second (markup-class-for-html chunk)))
+         (paragraph? (paragraph? chunk)))
+    (cond ((and paragraph? markup)
+           (values `(,markup (:P ,@block)) t))
+          (paragraph?
+           (values `(:P ,@block) t))
+          (markup
+           (values `(,markup ,@block) t))
+          (t
+           (values block nil)))))
+
+;;; ---------------------------------------------------------------------------
+
+(defmethod markup-class-for-html ((chunk chunk))
+  (when (markup-class chunk)
+    (let ((translation (item-at-1 *markup->html* (markup-class chunk))))
+      (unless translation 
+        (warn "No translation for '~A'" (markup-class chunk)))
+      translation)))
 
 ;;; ---------------------------------------------------------------------------
 
 (defmethod render-to-html ((chunk list))
-  (let ((translation (item-at-1 *markup->html* (first chunk))))
-    (unless translation
-      (warn "Unknown translation ~A" (first chunk)))
-    (safe-print (second translation))
-    (safe-print (second chunk))
-    (safe-print (third translation))))
-  
-;;; ---------------------------------------------------------------------------
-
-(defun start-markup (chunk)
-  (let ((translation (item-at-1 *markup->html* (markup-class chunk)))
-        (html-depth (size *html-environment*)))
-    (unless translation
-      (warn "Unknown translation ~A" (markup-class chunk)))
-    ;(format t "~%~D ~D ~A : " (level chunk) html-depth translation) 
-    
-    ;; Unless more of the same, do nothing...
-    (unless (and (= (1+ (level chunk)) html-depth)
-                 (equal translation (first *html-environment*))) 
-      (cond ((< (level chunk) html-depth)
-             ;; back out...
-             (loop repeat (- html-depth (level chunk)) do
-                   (pop-markup)))
-            ((>= (level chunk) html-depth) 
-             (when (first translation)
-               (push translation *html-environment*))
-             (safe-print (first translation)))))
-    
-    (safe-print (second translation))
-    (values (third translation))))
-  
-;;; ---------------------------------------------------------------------------
-
-(defun pop-markup ()
-  (assert (not (empty-p *html-environment*)))
-  (let ((translation (pop *html-environment*)))
-    (safe-print (fourth translation))))
+  (render-span-to-html (first chunk) (rest chunk)))
 
 ;;; ---------------------------------------------------------------------------
 
-(defmethod render-to-html ((line string))
-  (safe-print line)
-  ;;?? bit of a hack
-  (safe-print " "))
+(defmethod render-to-html ((chunk string))
+  ;;?? unlovely
+  (format nil "~A" chunk))
 
 ;;; ---------------------------------------------------------------------------
 
-(defun safe-print (thing)
-  (when thing
-    (lml2:lml-princ thing)))
-                           
+(defmethod render-span-to-html ((code (eql 'strong)) body)
+  `(:strong ,@body))
+
+;;; ---------------------------------------------------------------------------
+
+(defmethod render-span-to-html ((code (eql 'mail)) body)
+  (let ((address (first body)))
+    `((:a :href ,(format nil "mailto:~A" address)) ,address)))
+
+;;; ---------------------------------------------------------------------------
+
+(defmethod render-span-to-html ((code (eql 'emphasis)) body)
+  `(:em ,@body))
+
+;;; ---------------------------------------------------------------------------
+
+(defmethod render-span-to-html ((code (eql 'strong-em)) body)
+  `(:strong (:em ,@body)))
+
+;;; ---------------------------------------------------------------------------
+
+(defmethod render-span-to-html ((code (eql 'code)) body)
+  `(:code ,(render-to-html (first body))))
+
+;;; ---------------------------------------------------------------------------
+
+(defmethod render-span-to-html ((code (eql 'entity)) body)
+  (first body))
+
+;;; ---------------------------------------------------------------------------
+
+(defmethod render-span-to-html ((code (eql 'reference-link)) body)
+  (bind (((text &optional (id text)) body)
+         (link-info (item-at-1 (link-info *current-document*) id)))
+    (if link-info
+      `((:a :href ,(url link-info) ,@(awhen (title link-info) `(:title ,it)))
+        ,text)
+      `,text)))
+
+;;; ---------------------------------------------------------------------------
+
+(defmethod render-span-to-html ((code (eql 'inline-link)) body)
+  (bind (((text  &optional (url "") title) body))
+    `((:a :href ,url ,@(awhen title `(:title ,it)))
+      ,text)))
+
+;;; ---------------------------------------------------------------------------
+
+(defmethod render-span-to-html ((code (eql 'link)) body)
+  (bind ((url body))
+    `((:a :href ,@url) ,@url)))
+
+;;; ---------------------------------------------------------------------------
+
+(defmethod render-span-to-html ((code (eql 'html)) body)
+  (html-encode:encode-for-pre (first body)))
+
+
+
+
