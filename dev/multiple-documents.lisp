cfa76b06f7ffe1423f9b0e0f82e505c2f167e81b
Gary King <gwking@metabang.com>
2007-06-23 14:54:49 -0400

    Added multiple-documents.lisp
    darcs-hash:20070623185449-3cc5d-29a249cff0072a376ef435dcdd4b2480b1ca8a30.gz

---
+++ dev/multiple-documents.lisp
@@ -0,0 +1,69 @@
+(in-package #:cl-markdown)
+
+(defun markdown-many (pairs &rest args &key format &allow-other-keys)
+  (let ((main-document (make-instance 'document))
+	(docs nil))
+    (setf docs
+	  (loop for (source destination) in pairs collect
+	       (list (apply #'markdown source
+			    :parent main-document
+			    :format :none args)
+		     destination)))
+    ;; transfer information from docs to the parent
+    (loop for (doc destination) in docs do
+	 (transfer-document-data main-document doc destination))
+    ;; render 'em
+    (loop for (doc destination) in docs do
+	 (let ((*current-document* doc))
+	   (render-to-stream doc format destination)))
+    (values main-document docs)))
+
+#+(or)
+(markdown-many 
+ `((,(system-relative-pathname 'cl-markdown "dev/md1.md") 
+     ,(system-relative-pathname 'cl-markdown "dev/md1.html")) 
+   (,(system-relative-pathname 'cl-markdown "dev/md2.md") 
+     ,(system-relative-pathname 'cl-markdown "dev/md2.html")))
+ :format :html)
+
+(defun transfer-document-data (parent child destination)
+  ;; links
+  (let ((*current-document* parent))
+    (iterate-key-value
+     (link-info child)
+     (lambda (id info)
+       (setf (item-at (link-info parent) id)
+	     (transfer-link-info info parent child destination))))
+    ;; properties
+    (iterate-key-value
+     (properties child)
+     (lambda (key value)
+       (setf (document-property key) value)))))
+
+(defgeneric transfer-link-info (info parent child destination))
+
+(defmethod transfer-link-info ((info link-info) parent child destination)
+  (declare (ignore parent child destination))
+  (make-instance 'link-info
+                 :id (id info)
+		 :url (if (relative-url-p (url info))
+			  (format nil "~a~a" destination (url info))
+			  (url info))
+		 :title (title info)))
+
+(defun relative-url-p (url)
+  ;; FIXME -- look at the spec...
+  (not 
+   (or (starts-with url "http:")
+       (starts-with url "mailto:")
+       (starts-with url "file:"))))
+ 
+(defmethod transfer-link-info ((info extended-link-info)
+			       parent child destination)
+  (declare (ignore parent child destination))
+  (make-instance 'extended-link-info
+                 :id (id info)
+		 :kind (kind info)
+		 :contents (contents info)))
+
+
