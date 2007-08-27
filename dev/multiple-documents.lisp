e9b0d6c532cc027d93f15aaa4675e6ef02588671
Gary King <gwking@metabang.com>
2007-08-27 14:29:18 +0000

    Many improvements to multi-document support
    darcs-hash:606c78c8cf6539c212e46931c7870631d77a8d34

--- dev/multiple-documents.lisp
+++ dev/multiple-documents.lisp
@@ -1,25 +1,53 @@
 (in-package #:cl-markdown)
 
-(defun markdown-many (pairs &rest args &key format &allow-other-keys)
+(defun markdown-many (pairs &rest args 
+		      &key format additional-extensions render-extensions
+		      &allow-other-keys)
   (let ((main-document (make-instance 'document))
 	(docs nil))
     (setf docs
-	  (loop for (source destination) in pairs collect
-	       (list (apply #'markdown source
-			    :parent main-document
-			    :format :none args)
-		     destination)))
+	  (loop for datum in pairs collect
+	       (bind (((source destination &rest doc-args) datum))
+		 (format t "~&Parsing: ~s~%" source)
+		 (list (apply #'markdown source
+			      :parent main-document
+			      :format :none (append doc-args args))
+		       destination))))
     ;; transfer information from docs to the parent
     (loop for (doc destination) in docs do
-	 (transfer-document-data main-document doc destination))
+	   (transfer-document-data main-document doc destination))
     ;; render 'em
     (loop for (doc destination) in docs do
-	 (let ((*current-document* doc))
+	 (format t "~&Rendering: ~s" destination)
+	 (let ((*current-document* doc)
+	       (*render-active-functions*
+		(mapcar #'canonize-command
+			(or render-extensions
+			    (if additional-extensions
+				`(,@additional-extensions
+				  ,@*render-active-functions*)
+				*render-active-functions*)))))
 	   (render-to-stream doc format destination)))
     (values main-document docs)))
 
+(defun _render-one (doc)
+  (let ((*current-document* doc)
+	(*render-active-functions*
+	 (mapcar #'canonize-command
+		 `(cl-markdown::docs cl-markdown::docs-index
+				     cl-markdown::today cl-markdown::now
+				     cl-markdown::glossary
+				     ,@*render-active-functions*))))
+    (render-to-stream doc :html #p"/tmp/one.html")))
+
 #+(or)
-(markdown-many 
+(untrace markdown)
+    
+#+(or)
+(compile 'markdown-many)
+
+#+(or)
+(cl-markdown:markdown-many 
  `((,(system-relative-pathname 'cl-markdown "dev/md1.md") 
      ,(system-relative-pathname 'cl-markdown "dev/md1.html")) 
    (,(system-relative-pathname 'cl-markdown "dev/md2.md") 
@@ -38,7 +66,30 @@
     (iterate-key-value
      (properties child)
      (lambda (key value)
-       (setf (document-property key) value)))))
+       (when (transfer-property-p child key)
+	 (setf (document-property key) (first value)))))))
+
+(defgeneric transfer-property-p (document property))
+
+(defmethod transfer-property-p (document property) 
+  (declare (ignore property document))
+  (values t))
+
+(defmethod transfer-property-p (document (property (eql :footnote)))
+  (declare (ignore document))
+  (values nil))
+
+(defmethod transfer-property-p (document (property (eql :style-sheet)))
+  (declare (ignore document))
+  (values nil))
+
+(defmethod transfer-property-p (document (property (eql :style-sheetshtml)))
+  (declare (ignore document))
+  (values nil))
+
+(defmethod transfer-property-p (document (property (eql :title)))
+  (declare (ignore document))
+  (values nil))
 
 (defgeneric transfer-link-info (info parent child destination))
 
@@ -47,7 +98,10 @@
   (make-instance 'link-info
                  :id (id info)
 		 :url (if (relative-url-p (url info))
-			  (format nil "~a~a" destination (url info))
+			  (format nil "~a~@[.~a~]~a" 
+				  (pathname-name destination)
+				  (pathname-type destination)
+				  (url info))
 			  (url info))
 		 :title (title info)))
 
