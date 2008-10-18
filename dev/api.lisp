671cb13f691e44ef501a34520ee33194ff6c6725
Gary King <gwking@metabang.com>
2008-10-18 18:58:45 -0400

    Added defgenerics; someday I'll add documentation too! sigh.
    darcs-hash:20081018225845-3cc5d-79249d20c6985057247fc6e5a0ac201b15dca2c5.gz

--- dev/api.lisp
+++ dev/api.lisp
@@ -1,8 +1,81 @@
 (in-package #:cl-markdown)
 
+
+(defgeneric reset (thing)
+  )
+
+
+(defgeneric (setf document-property) (value name))
+
+(defgeneric render-to-stream (document style stream-specifier)
+  )
+
+(defgeneric main-parent (document)
+  )
+
+(defgeneric handle-spans (document)
+  )
+
+(defgeneric scan-one-span (what scanner-name scanner scanners)
+  )
+
+(defgeneric process-span-in-span-p (sub-span current-span)
+  )
+
+(defgeneric unconvert-escapes (what)
+  )
+
+(defgeneric render (document style stream)
+  )
+
+(defgeneric it-starts-with-block-level-html-p (chunk)
+  )
+
+(defgeneric markup-class-mergable-p (what)
+  )
+
+(defgeneric merge-lines-in-chunks (what)
+  )
+
+(defgeneric can-merge-lines-p (first second)
+  )
+
+(defgeneric handle-paragraph-eval-interactions (what)
+  )
+
+(defgeneric encode-html (what encoding-method &rest codes)
+  )
+
+(defgeneric markup-class-for-html (what)
+  )
+
+(defgeneric render-span-to-html (kind body encoding-method)
+  )
+
+(defgeneric generate-link-output (link-info text)
+  )
+
+(defgeneric add-html-header-p (document)
+  )
+
+(defgeneric render-plain (what)
+  )
+
+(defgeneric render-span-plain (kind body)
+  )
+
+(defgeneric process-span-for (kind command args)
+  )
+
+(defgeneric generate-link-output-for-kind (kind link-info text)
+  )
+
 (defgeneric process-span (name registers)
   (:documentation "Called during span processing on each match of name in the
 document. Registers a list of the registers captured by names regular expression.
 Returns a possibly new set of registers.")
   (:method ((name t) (registers t))
            (values registers)))
+
+(defgeneric print-html-markup (markup stream)
+  )
