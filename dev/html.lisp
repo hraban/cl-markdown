ec2e086586e901369a7206154eb9dc90e7d3b922
Gary King <gwking@metabang.com>
2009-04-26 22:27:43 -0400

    s/HTML/html
    darcs-hash:20090427022743-3cc5d-51fe459051bbd4e71b48bee753868abd3902e7f7.gz

--- dev/html.lisp
+++ dev/html.lisp
@@ -459,7 +459,7 @@
 
 (defun generate-doctype ()
   (format *output-stream* 
-	  "~&<!DOCTYPE HTML PUBLIC ~s ~s>"
+	  "~&<!DOCTYPE html PUBLIC ~s ~s>"
 	  "-//W3C//DTD XHTML 1.0 Strict//EN"
 	  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"))
 
